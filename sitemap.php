91537915edd8480788268fea9db2e70dc441983e
fHpro0 <hoernerfrederick@gmail.com>
2021-12-15 11:49:59 +0100

    Sitemap aktualisiert

--- sitemap.php
+++ sitemap.php
@@ -5,9 +5,12 @@ include "./templates/head.php";
 
 <div class="container">
     <h1>Sitemap</h1>
-    <ul>
-        <li></li>
-    </ul>
+    <ul class="">
+        <li class=""><a href="/index" class="nav-link px-2 text-muted">Index</a></li>
+        <li class=""><a href="" class="nav-link px-2 text-muted">Geraetverwaltung</a></li>
+        <li class=""><a href="/impressum" class="nav-link px-2 text-muted">Impressum</a></li>
+        <li class=""><a href="/sitemap" class="nav-link px-2 text-muted">Sitemap</a></li>
+    </ul>  
 </div>
 
 <?php include "./templates/footer.php"; ?>
